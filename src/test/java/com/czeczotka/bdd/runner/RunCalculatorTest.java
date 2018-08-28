50572ec034be23f02af1ecd0410f45ac4ac4f403
dhaivatvanani <39957467+dhaivatvanani@users.noreply.github.com>
2018-08-28 18:54:09 +0530

    Update RunCalculatorTest.java

--- src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
+++ src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		  features = "classpath:cucumber/New.feature" ,
+		  features = "classpath:cucumber/Requirementlink.feature" ,
 		  glue = "com.czeczotka.bdd.steps",
 		plugin = {"json:target/Cucumber.json"},
 		monochrome = true
